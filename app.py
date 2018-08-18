e3c6f4c1d18cc6acd56f988a0bb2c4350dc5fadd
Lukas Reimer <lukas.reimer@icloud.com>
2018-08-18 16:00:50 +0200

    added actions and tool bar to main app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import random
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
@@ -22,20 +23,45 @@ class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Main App")
+
         self.status_bar = QtWidgets.QStatusBar()
         self.setStatusBar(self.status_bar)
-        self.status_bar.showMessage("Initializing...")
 
         self.scene = QtWidgets.QGraphicsScene()
-
         self.view = MyView(self.scene)
         self.view.setMinimumWidth(400)
         self.view.setMinimumHeight(300)
         self.view.setBackgroundBrush(QtGui.QBrush(QtCore.Qt.darkGray))
-
         self.setCentralWidget(self.view)
 
+        # Actions:
+        self.add_node_action = QtWidgets.QAction("Add Node", parent=self)
+        self.add_node_action.triggered.connect(self.add_node)
+        self.zoom_in_action = QtWidgets.QAction("Zoom in", parent=self)
+        self.zoom_in_action.triggered.connect(self.zoom_in)
+        self.zoom_out_action = QtWidgets.QAction("Zoom out", parent=self)
+        self.zoom_out_action.triggered.connect(self.zoom_out)
+
+        # Tool Bar:
+        self.tool_bar = QtWidgets.QToolBar("Main Tool Bar")
+        self.tool_bar.addAction(self.add_node_action)
+        self.tool_bar.addAction(self.zoom_in_action)
+        self.tool_bar.addAction(self.zoom_out_action)
+        self.addToolBar(self.tool_bar)
+
         self.status_bar.showMessage("Ready")
+
+    def add_node(self):
+        x = random.randint(-200, 200)
+        y = random.randint(-150, 150)
+        self.scene.addEllipse(x, y, 10, 10)
+
+    def zoom_in(self):
+        self.view.zoom_in()
+
+    def zoom_out(self):
+        self.view.zoom_out()
 
 
 def main():
