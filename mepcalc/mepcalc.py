a562f789825ff88b3c999b048fb1b4749d8f42e0
Lukas Reimer <lukas.reimer@icloud.com>
2022-11-22 21:15:18 +0100

    added qalculator widgets to mepcalc gui

--- mepcalc/mepcalc.py
+++ mepcalc/mepcalc.py
@@ -3,6 +3,8 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QTabWidget
+from qalculator_gui_mass import QalculatorMassWidget
+from qalculator_gui_volume import QalculatorVolumeWidget
 
 
 class MEPCalc(QMainWindow):
@@ -16,18 +18,32 @@ class MEPCalc(QMainWindow):
 
     def setup_ui(self):
         """Setup User Interface."""
+        self.create_calculations_widgets()
+        self.setup_tab_interface()
+
+    def create_calculations_widgets(self):
+        """Create low level calculation widgets."""
+        self.qalculator_mass = QalculatorMassWidget()
+        self.qalculator_volume = QalculatorVolumeWidget()
+
+    def setup_tab_interface(self):
+        """Setup multilayer tabbed interface."""
         # Second Level Tabs
         heat_flow_tabs = QTabWidget()
-        heat_flow_tabs.addTab(QLabel("Heat Flow: Mass Flow"), "Mass Flow")
-        heat_flow_tabs.addTab(QLabel("Heat Flow: Volume Flow"), "Volume Flow")
+        heat_flow_tabs.tabBar().setDocumentMode(True)
+        heat_flow_tabs.addTab(self.qalculator_mass, "Mass Flow")
+        heat_flow_tabs.addTab(self.qalculator_volume, "Volume Flow")
         duct_tabs = QTabWidget()
+        duct_tabs.tabBar().setDocumentMode(True)
         duct_tabs.addTab(QLabel("Duct: Rectangular"), "Rectangular")
         duct_tabs.addTab(QLabel("Duct: Round"), "Round")
         pipe_tabs = QTabWidget()
+        pipe_tabs.tabBar().setDocumentMode(True)
         pipe_tabs.addTab(QLabel("Pipe: Pressure"), "Pressure")
         pipe_tabs.addTab(QLabel("Pipe: Gravitation"), "Gravitation")
         # Top Level Tabs
         main_tabs = QTabWidget(self)
+        main_tabs.tabBar().setDocumentMode(True)
         main_tabs.addTab(heat_flow_tabs, "Heat Flow")
         main_tabs.addTab(duct_tabs, "Duct")
         main_tabs.addTab(pipe_tabs, "Pipe")
